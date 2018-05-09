454c6a46bb1f6fcc8096ab5ece549b3276841f7f
orionlee <orionlee@yahoo.com>
2018-05-09 14:05:37 -0700

    Experiment automatic feed update: retry feed update if network is not available (wait till network is available again)

--- core/src/main/java/de/danoeh/antennapod/core/receiver/FeedUpdateReceiver.java
+++ core/src/main/java/de/danoeh/antennapod/core/receiver/FeedUpdateReceiver.java
@@ -3,7 +3,12 @@ package de.danoeh.antennapod.core.receiver;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
 import android.os.Handler;
+import android.util.Log;
+
+import java.util.concurrent.Callable;
 
 import de.danoeh.antennapod.core.ClientConfig;
 import de.danoeh.antennapod.core.preferences.UserPreferences;
@@ -43,7 +48,79 @@ public class FeedUpdateReceiver extends BroadcastReceiver {
             new Handler().postDelayed(NetworkUtils::networkAvailable, 2000);
             new Handler().postDelayed(NetworkUtils::networkAvailable, 4000);
             new Handler().postDelayed(NetworkUtils::networkAvailable, 6000);
+
+            // Refresh all feeds once network is available
+            // MUST supply application context, receiver's own context is now allowed to be used.
+            OnNetworkAvailableOneTimeReceiver.run(context.getApplicationContext(),
+                    () -> refreshAllFeedsIfDownloadAllowed(context));
         }
     }
 
+    private static boolean refreshAllFeedsIfDownloadAllowed(Context context) {
+        if (NetworkUtils.isDownloadAllowed()) {
+            DBTasks.refreshAllFeeds(context, null);
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+class OnNetworkAvailableOneTimeReceiver extends BroadcastReceiver {
+
+    private static final String TAG = "OnNetAvailOneTimeRcvr";
+
+    private final Callable<Boolean> callable;
+
+    /**
+     *
+     * @param context
+     * @param callable the logic to be executed when network is available. It returns true upon success,
+     *                 false if the execution does not complete, and wishes to retry
+     *                 when network is available again.
+     *                 For example, the logic refuses to run on a metered network.
+     */
+    public static void run(Context context, Callable<Boolean> callable) {
+        // OPEN: consider to avoid run() being called repeatedly (before network becomes available)
+        LogToFile.d(context, TAG, "run() - registering network change receiver");
+
+        BroadcastReceiver receiver = new OnNetworkAvailableOneTimeReceiver (callable);
+        IntentFilter intentFilter = new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION);
+        context.registerReceiver(receiver, intentFilter);
+    }
+
+    private OnNetworkAvailableOneTimeReceiver(Callable<Boolean> callable) {
+        this.callable = callable;
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        LogToFile.d(context, TAG, "onReceive() - intent: " + intent);
+        // verify intent is for network availability
+        if (intent == null && !ConnectivityManager.CONNECTIVITY_ACTION.equals(intent.getAction())) {
+            LogToFile.d(context, TAG, "Not a connectivity change intent. Do nothing");
+            return;
+        }
+
+        // Network not available (probably a broadcast of disconnect), ignore
+        if (!NetworkUtils.networkAvailable()) {
+            LogToFile.d(context, TAG, "No network available. Do nothing");
+            return;
+        }
+
+        boolean success = true;
+        try {
+            LogToFile.d(context, TAG, "Network available - execute supplied logic");
+            success = callable.call();
+        } catch (Exception e) {
+            Log.e(TAG, "Unexpected error in executing the logic. ", e); // No retry with exception
+        } finally {
+            if (success) {
+                LogToFile.d(context, TAG, "Supplied logic executed successfully - Now un-registering network change receiver");
+                context.unregisterReceiver(this);
+            } else {
+                LogToFile.d(context, TAG, "Supplied logic executed unsuccessfully - Retry when network is available again");
+            }
+        }
+    }
 }
