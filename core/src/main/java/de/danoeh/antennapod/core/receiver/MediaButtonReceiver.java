8bb720b374ded40db65a546db06e4a61ca515a09
orionlee <orionlee@yahoo.com>
2019-03-10 13:22:15 -0700

    MediaButtonReceiver - mimic more closely to support lib to give button handler more leeway (with respect to BroadcastReceiver time limit), with goAsync() and handler.post()

--- core/src/main/java/de/danoeh/antennapod/core/receiver/MediaButtonReceiver.java
+++ core/src/main/java/de/danoeh/antennapod/core/receiver/MediaButtonReceiver.java
@@ -5,6 +5,7 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.os.Handler;
 import android.os.IBinder;
 import android.support.annotation.NonNull;
 import android.util.Log;
@@ -29,8 +30,11 @@ public class MediaButtonReceiver extends BroadcastReceiver {
 		}
 		KeyEvent event = (KeyEvent) intent.getExtras().get(Intent.EXTRA_KEY_EVENT);
 		sendKeyEvent(context, event);
+		Log.v(TAG, "onReceive() returned.");
 	}
 
+	// This mimics how support lib MediaButtonReceiver works
+	// , with the underlying MediaBrowserCompat.connect() and the underlying MediaBrowser.connect()
 	private void sendKeyEvent(Context context, KeyEvent event) {
 		if (event == null || event.getAction() != KeyEvent.ACTION_DOWN || event.getRepeatCount() != 0) {
 			return;
@@ -39,56 +43,72 @@ public class MediaButtonReceiver extends BroadcastReceiver {
 		// MUST use applicationContext, BroadcastReceiver's context does not allow bindService
 		final Context appContext = context.getApplicationContext();
 
-		// This mimics how MediaBrowserCompat.connect(), and the underlying MediaBrowser.connect(), work,
-		// A difference is that MediaBrowser.connect() wraps the service binding logic as a Runnable
-		// and call it from mHandler.post()
-			Log.d(TAG, "Sending KeyEvent to PlaybackService: " + event);
+		Log.d(TAG, "Sending KeyEvent to PlaybackService: " + event);
+		Handler mainThreadHandler = new Handler();
+		PendingResult pendingResult = goAsync(); // mimic support lib MediaButtonReceiver.onReceive()
+		// Mimic MediaBrowserCompat.connect() and the underlying MediaBrowser.connect(), i.e.,
+		// binding the service in a handler.post() call
+		mainThreadHandler.post(() -> {
+			// use mainThreadHandler.post() to ensure service binding is done in main thread, in case goAsync() moves the ops to a different thread
+			Log.v(TAG, "in Handler.post() start");
 			try {
 				ClientConfig.initialize(appContext);
 				Intent serviceIntent = new Intent(appContext, PlaybackService.class);
 				boolean bound = appContext.bindService(serviceIntent,
-						new MediaButtonServiceConnection(appContext, event.getKeyCode()),
+						new MediaButtonServiceConnection(appContext, event.getKeyCode(), pendingResult),
 						Context.BIND_AUTO_CREATE);
 				if (!bound) {
 					Log.e(TAG, "Unable to passing media button to PlaybackService - failed to bind to the service");
+					pendingResult.finish();
 				}
 			} catch (Throwable t) {
 				Log.e(TAG, "Unable to passing media button to PlaybackService - Unexpected exception", t);
+				pendingResult.finish();
 			}
+			Log.v(TAG, "in Handler.post() end");
+		});
 	}
 
 	private static class MediaButtonServiceConnection implements ServiceConnection {
 		@NonNull
 		private final Context context;
 		private final int keycode;
+		@NonNull
+		private final PendingResult pendingResult;
 
-		public MediaButtonServiceConnection(@NonNull Context context, int keycode) {
+		public MediaButtonServiceConnection(@NonNull Context context, int keycode, @NonNull PendingResult pendingResult) {
 			this.context = context;
 			this.keycode = keycode;
+			this.pendingResult = pendingResult;
 		}
 
 		@Override
 		public void onBindingDied(ComponentName name) {
 			Log.v(TAG, "onBindingDied() called with: name = [" + name + "]");
+			pendingResult.finish();
 		}
 
 		@Override
 		public void onServiceConnected(ComponentName name, IBinder serviceBinder) {
-			Log.v(TAG, "onServiceConnected() - serviceBinder is local:" + (serviceBinder instanceof PlaybackService.LocalBinder));
+			Log.v(TAG, "onServiceConnected() - starts.  thread:" +Thread.currentThread().getName());
 			try {
 				PlaybackService service = ((PlaybackService.LocalBinder) serviceBinder).getService();
 				service.handleKeyCode(keycode);
 				// if the keycode is unsupported, the service will provide proper response. No action is needed here
 			} finally {
-				context.unbindService(this);
+				Log.v(TAG, "onServiceConnected() - about to unbind and pendingResult.finish()");
+				try {
+					context.unbindService(this);
+				} finally {
+					pendingResult.finish();
+				}
 			}
-
 		}
 
 		@Override
 		public void onServiceDisconnected(ComponentName name) {
 			Log.v(TAG, "onServiceDisconnected() called with: name = [" + name + "]");
-
+			pendingResult.finish();
 		}
 	}
 
