52fb851331e87898aacbc94c310c2a74d34dfdea
orionlee <orionlee@yahoo.com>
2019-10-04 14:04:57 -0700

    Enqueue tweaks - rename DownloadStateProvider instances to be consistent with the new name

--- core/src/test/java/de/danoeh/antennapod/core/storage/ItemEnqueuePositionCalculatorTest.java
+++ core/src/test/java/de/danoeh/antennapod/core/storage/ItemEnqueuePositionCalculatorTest.java
@@ -186,7 +186,7 @@ public class ItemEnqueuePositionCalculatorTest {
             //
             ItemEnqueuePositionCalculator calculator = new ItemEnqueuePositionCalculator(options);
             MockDownloadRequester mockDownloadRequester = new MockDownloadRequester();
-            calculator.requester = mockDownloadRequester;
+            calculator.downloadStateProvider = mockDownloadRequester;
 
             // Setup initial data
             // A shallow copy, as the test code will manipulate the queue
